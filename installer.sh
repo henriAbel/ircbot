99d95a241ad28db267a16f965035c59e38da6c0c
root <henriabel@gmail.com>
2014-01-07 09:36:34 +0200

    Installer bugfix

--- installer.sh
+++ installer.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
    exit 1
 fi
 
 apt-get -y install python-pip python-flup python-mysqldb python-twisted python-imaging
 
-pip install Django==1.5.4
-pip install apscheduler
+sudo pip install Django==1.5.4
+sudo pip install apscheduler
