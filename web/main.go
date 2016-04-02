f2e5fe16b858300e42bce7d450252dccb063cf0a
Henri Abel <henriabel@gmail.com>
2016-04-02 14:12:12 +0300

    Support https

--- web/main.go
+++ web/main.go
@@ -1,15 +1,42 @@
 package web
 
 import (
+	"crypto/rand"
+	"crypto/tls"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"strings"
 	"time"
 
 	irc "../irc"
 	"github.com/StephanDollberg/go-json-rest-middleware-jwt"
 	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/soheilhy/cmux"
 )
+
+func serveHTTP1(l net.Listener, m *http.ServeMux) {
+	s := &http.Server{
+		Handler: m,
+	}
+	if err := s.Serve(l); err != cmux.ErrListenerClosed {
+		panic(err)
+	}
+}
+
+func serveHTTPS(l net.Listener, m *http.ServeMux) {
+	certificate, err := tls.LoadX509KeyPair(irc.GetCertFile(), irc.GetKeyFile())
+	if err != nil {
+		log.Panic(err)
+	}
+	config := &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+		Rand:         rand.Reader,
+	}
+	tlsl := tls.NewListener(l, config)
+	serveHTTP1(tlsl, m)
+}
 
 func StartWeb() {
 	var DefaultDevStack = []rest.Middleware{
@@ -59,9 +86,37 @@ func StartWeb() {
 	)
 	api.SetApp(apiRouter)
 
-	http.Handle("/api/", http.StripPrefix("/api", api.MakeHandler()))
-	http.Handle("/bower_components/", http.FileServer(http.Dir("./web/static/")))
-	http.Handle("/", http.FileServer(http.Dir("./web/static/app/")))
 	listeningPort := fmt.Sprintf(":%d", irc.GetWebPort())
-	log.Fatal(http.ListenAndServe(listeningPort, nil))
+	enableTls := len(irc.GetCertFile()) > 0 && len(irc.GetKeyFile()) > 0
+	mux := http.NewServeMux()
+	mux.Handle("/api/", http.StripPrefix("/api", api.MakeHandler()))
+	mux.Handle("/bower_components/", http.FileServer(http.Dir("./web/static/")))
+	mux.Handle("/", http.FileServer(http.Dir("./web/static/app/")))
+
+	l, err := net.Listen("tcp", listeningPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := cmux.New(l)
+
+	httpl := m.Match(cmux.HTTP1Fast())
+	if enableTls {
+		tlsl := m.Match(cmux.Any())
+		redirectMux := http.NewServeMux()
+		redirectMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			u := r.URL
+			u.Host = r.Host
+			u.Scheme = "https"
+			http.Redirect(w, r, u.String(), http.StatusMovedPermanently)
+		})
+		go serveHTTPS(tlsl, mux)
+		go serveHTTP1(httpl, redirectMux)
+	} else {
+		go serveHTTP1(httpl, mux)
+	}
+
+	if err := m.Serve(); !strings.Contains(err.Error(), "use of closed network connection") {
+		panic(err)
+	}
+
 }
